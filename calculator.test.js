1096c90fef1a26f770d80394bde2edff8f687a7c
Baidaa Albzoor <baidaaalbzoor@outlook.com>
2018-09-04 08:26:43 +0300

    Add 'unknown amount of numbers' test case

--- calculator.test.js
+++ calculator.test.js
@@ -11,3 +11,7 @@ test('return 1 if input is "1"', () => {
 test('return 3 if input is "1,2"', () => {
     expect(add("1,2")).toBe(3);
 });
+
+test('handle unknown amount of numbers', () => {
+    expect(add("1,2,3")).toBe(6);
+});
