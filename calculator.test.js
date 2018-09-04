6f0900ef1604d9a65a53c83621a13230f139993d
Baidaa Albzoor <baidaaalbzoor@outlook.com>
2018-09-04 10:40:39 +0300

    Add 'multiple delimiters' test case

--- calculator.test.js
+++ calculator.test.js
@@ -33,3 +33,12 @@ test('throw an exception if input is a negative number', () => {
 test('return 2 if input is "2,1001"', function () {
     expect(add("2,1001")).toBe(2);
 });
+
+
+test('returns 6 when given "//[]\n12**3"', function () {
+    expect(add("//[]\n12**3")).toBe(6);
+});
+
+test('returns 6 when given "//[][%]\n1*2%3"', function () {
+    expect(add("//[][%]\n1*2%3")).toBe(6);
+});
